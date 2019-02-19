57dca00036bd2ac42dd3a3855bbbc6593a7636c5
M.Gurung <mg636@kent.ac.uk>
2019-02-19 13:37:19 +0000

    Made tea biscuits changes to bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,18 @@
-function answerQuery(query) {
-  query = query.toLowerCase();
-  if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
+function answerQuery(message) {
+	if (message.indexOf("rain") != -1 || message.indexOf("sun") != -1 || message.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
-  return "Sorry Dave, I cannot do that."
+     else if (message.indexOf("tea") != -1 || message.indexOf("biscuit") != -1)
+    return "Ronnie Pickering is not my father.";
+return "Sorry Dave, I cannot do that.";
 }
 
-
-function handleSayClick() {
-  // We first get the 'message' that the user entered and 'conversation' element
-  // which we are using for showing all the conversation history
+function saySomething() {
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
+  var answer = answerQuery(message);
+
+ 
   
-  // Get answer for the given query and show the query & answer
-  var answer = answerQuery(message)
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
   conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
 }
